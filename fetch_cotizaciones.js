27c0abc5de7c0d48736c094581382a154878aa61
Mariano Szklanny <mszklanny@users.noreply.github.com>
2019-02-12 20:31:53 -0300

    Added threshold identification logic

--- fetch_cotizaciones.js
+++ fetch_cotizaciones.js
@@ -1,34 +1,40 @@
-const request = require('request')
+const request = require('request');
 
 module.exports = function(ctx, cb) {
-    
-    
-request.post('https://api.ibcambio.com/api/gettoken/', {
-  json: {
-    usuario: ctx.secrets.usuario,
-    contrasena: ctx.secrets.contrasena
-  }
-}, (error, res, body) => {
-  if (error) {
-    console.error(error)
-    cb(error, null)
-    return
-  }
-  var token = body
+
+  ctx.storage.get(function (error, thresholds) {
+    if (error) {
+      return cb(error);
+    }
+        
+   request.post('https://api.ibcambio.com/api/gettoken/', {
+      json: {
+        usuario: ctx.secrets.usuario,
+        contrasena: ctx.secrets.contrasena
+      }
+    }, (err, res, body) => {
+    if (err) {
+      return cb(err, null);
+    }
+    var token = body;
   
-  request.get('https://api.ibcambio.com/api/cotizaciones/', {headers: token},
-    (error, res, body) => {
-        if (error) {
-        console.error(error)
-        return
-        }
-        console.log(`statusCode cotizaciones: ${res.statusCode}`)
-        const json = JSON.parse(body)
-        var cotizacion = json.Cotizaciones.find(function(element) {return element.plazo == '24hs.'})
-        cb(null, cotizacion)
-    })
-})
-    
+    request.get('https://api.ibcambio.com/api/cotizaciones/', {headers: token}, (error, res, body) => {
+          if (error) {
+            return cb(error, null);
+          }
+          console.log(`statusCode cotizaciones: ${res.statusCode}`);
+          const json = JSON.parse(body);
+          var cotizacion = json.Cotizaciones.find(function(element) {return element.plazo === '24hs.'});
+          if (cotizacion.Compra >= thresholds.Compra) {
+            return cb(null, "Vender dólares a " + cotizacion.Compra + ". Hábil? " + json.Habil);
+          }
+          if (cotizacion.Venta <= thresholds.Venta) {
+            return cb(null, "Comprar dólares a " + cotizacion.Venta + ". Hábil? " + json.Habil);
+          }
+          return cb(null, cotizacion);
+      });
+    });
+  });
 }
 
 
