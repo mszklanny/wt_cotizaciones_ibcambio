c3a800f868fd19785716aebd2890b33bc594e68a
Mariano Szklanny <mszklanny@gmail.com>
2019-05-12 11:34:21 -0300

    Added descriptive results.

--- dolar_alert_ibcambio.js
+++ dolar_alert_ibcambio.js
@@ -8,6 +8,10 @@ const nodemailer = require('nodemailer');
 * In your mobile, configure an email filter and set a custom notification for it.
 * Up to one email per day is sent.
 * CRON settings: 1-59 10-17 * * 1-5
+*
+* Params
+* - venta: set the sell threshold. If the rate goes below this value, the alert will be triggered.
+* - clear_notification: set it to 1 to clear the "daily notification sent" flag.
 */
 module.exports = function(context, cb) {
   function getStorageData(callback) {
@@ -68,14 +72,25 @@ module.exports = function(context, cb) {
         if (json.Habil && cotizacion.Venta <= storage.venta) {
           sendEmail(cotizacion.Venta, storage);
         } else {
-          return cb(null, "Nothing done. The current rate ($" + cotizacion.Venta + ") has not reached the threshold ($" + storage.venta + ") or the market is closed at the moment (Habil = " + json.Habil + ").");
+          return cb(null, "Nothing done. The current rate ($" + cotizacion.Venta + ") has not reached the threshold ($" + storage.venta + ") or the market is closed at the moment (Habil = " + json.Habil + "). Last notification sent on: " + (typeof storage.last_notification !== 'undefined' ? storage.last_notification : "(not sent)") + ". To reset the notification, call with ?clear_notification=1. To set a new threshold, provide the parameter 'venta'.");
         }
     });
   }
+
   getStorageData(function (storage){
+    if (context.query.venta) {
+      storage.venta = context.query.venta;
+      context.storage.set(storage);
+      console.log("New threshold set.");
+    }
+    if (context.query.clear_notification) {
+      delete storage.last_notification;
+      context.storage.set(storage);
+      console.log("Last notification cleared.");
+    }
     if (storage.last_notification === new Date().toDateString())
     {
-      return cb(null, "Nothing done. The daily notification was already sent because the threshold ($" + storage.venta + ") was reached today.");
+      return cb(null, "Nothing done. The daily notification was already sent because the threshold ($" + storage.venta + ") was reached today. To reset the notification, call with ?clear_notification=1. To set a new threshold, provide the parameter 'venta'.");
     }
     return getToken(storage, getCotizaciones);
   });
